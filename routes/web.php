d176c2a67c5b40ece5dc8fe3eb4209ccfc44e051
hsnapps <34065394+hsnapps@users.noreply.github.com>
2020-11-18 15:18:20 +0300

    Changed monitor page language and direction

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::post('/login', 'Auth\LoginController@login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/screen/{id}', 'ScreenController@minitor')->name('monitor');
+Route::get('/s/{id}', 'ScreenController@minitor')->name('monitor2');
 
 Route::middleware(['auth'])->group(function () {
     Route::prefix('screens')->group(function () {
